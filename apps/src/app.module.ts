94897acd8ec921a03db124e5fd9fa611d31d3305
khusanraymjonov <khusan.raymjonov.b@gmail.com>
2026-08-20 21:27:32 +0100

    Delete apps/src directory

--- apps/src/app.module.ts
+++
@@ -1,6 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FoundationController } from './foundation.controller.js';
-import { TokenisationController } from './tokenisation.controller.js';
-
-@Module({ controllers: [FoundationController, TokenisationController] })
-export class AppModule {}
